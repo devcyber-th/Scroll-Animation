a77e9b39ee65374a65ec261fd83a2152ea16d144
Thiago Fernandes <devcyber.thiago@gmail.com>
2024-08-26 11:05:25 -0300

    atualização2

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const boxes = document.querySelectorAll(".box");
 
 window.addEventListener("scroll", checkBoxes);
-window.addEventListener("resize", checkBoxes); // Adiciona evento de resize para ajustar o triggerBottom
 
 function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 3.5; // Ajuste para dispositivos móveis
+  const triggerBottom = window.innerHeight / 5 * 4;
 
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top;
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top
 
-        if (boxTop < triggerBottom) {
-            box.classList.add('show');
-        } else {
-            box.classList.remove('show');
-        }
-    });
+    if(boxTop < triggerBottom) {
+        box.classList.add('show');
+    } else {
+        box.classList.remove('show')
+    }
+  })
 }
